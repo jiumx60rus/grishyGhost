857df39b034906abdc607932dc7768b44fc07814
Sergey <grishinygrada@gmail.com>
2015-08-18 17:12:14 +0300

    Blog update: Tue Aug 18 2015 17:12:14 GMT+0300 (MSK)

--- deploy.js
+++ deploy.js
@@ -39,7 +39,7 @@ var buster = exec('buster generate',
 
                 var commitText = "Blog update: " + new Date();
 
-                var push = exec('cp -r staticDecor/dist/ staticGit && cd staticGit && git add -A && git commit -m "' + commitText + '" && git push',
+                var push = exec('cp -r staticDecor/dist/ staticGit && cd staticGit && git add -A :/ && git commit -m "' + commitText + '" && git push',
                     function(error, stdout, stderr) {
                         console.log('stdout: ' + stdout);
                         console.log('stderr: ' + stderr);
@@ -47,6 +47,15 @@ var buster = exec('buster generate',
                             console.log('exec error: ' + error);
                         }
 
+                        var push = exec('git add -A :/ && git commit -m "' + commitText + '" && git push',
+                            function(error, stdout, stderr) {
+                                console.log('stdout: ' + stdout);
+                                console.log('stderr: ' + stderr);
+                                if (error !== null) {
+                                    console.log('exec error: ' + error);
+                                }
+
+                            });
                     });
             });
 
